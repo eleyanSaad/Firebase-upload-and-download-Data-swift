fca4a2a85a29cd76e24e260795a8164978c8f4f5
eleyan <elyansaad1994@gmail.com>
2019-06-30 20:17:50 +0300

    Initial Commit

---
+++ FirebaseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseExample
+//
+//  Created by Eleyan Saad on 6/30/19.
+//  Copyright © 2019 Eleyan Saad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
